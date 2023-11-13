72cdf48fe03e1339d95989a3fbcf4c3a03cad5fa
Gillys Ayres <gillys.ayres@gmail.com>
2023-11-13 11:13:53 -0300

    add searchResults component and wrapping everything in appjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import SearchForm from './components/searchForm/searchForm';
+import SearchResults from './components/searchResults/searchResults';
+import { SEARCH_LEGAL_CASES } from './services/api/queries';
+import jusbrasilLogo from './assets/images/jusbrasil-logo.svg';
 import './App.css';
 
 function App() {
+  const [recordsFound, setRecordsFound] = useState(true); // State to keep track of whether records were found
+
+  const [executeSearch, { loading, error, data }] = useLazyQuery(
+    SEARCH_LEGAL_CASES,
+    {
+      // Removing variables here since they will be set dynamically in handleFormSubmit
+      fetchPolicy: 'no-cache', // Ensuring fresh results on each search
+    }
+  );
+
+  const handleFormSubmit = async (cnjNumberFromForm) => {
+    // Log before executing the search
+    console.log('Executing search with CNJ Number:', cnjNumberFromForm);
+
+    try {
+      // Execute the search with the CNJ number provided from the form
+      const result = await executeSearch({
+        variables: { cnjNumber: cnjNumberFromForm },
+      });
+
+      // Check if data contains search results and update the state accordingly
+      if (
+        result.data &&
+        result.data.legalCases &&
+        result.data.legalCases.length > 0
+      ) {
+        // Results found, update the recordsFound state to true
+        setRecordsFound(true);
+      } else {
+        // No results found, update the recordsFound state to false
+        setRecordsFound(false);
+      }
+    } catch (fetchError) {
+      // Handle errors with the search (e.g., network issues, query errors)
+      console.error('Error fetching data: ', fetchError);
+      setRecordsFound(false); // Assume no records found if there's an error
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <img
+        src={jusbrasilLogo}
+        alt="Jusbrasil Logo"
+        className="jusbrasil-logo"
+      />
+      <header className="app-header">
+        <h1 className="app-title">Consulta Processual</h1>
       </header>
+      <SearchForm handleSearch={handleFormSubmit} />
+      {loading && <p>Carregando...</p>}
+      {!loading && !recordsFound && (
+        <p>Nenhum processo com esse número CNJ foi encontrado.</p>
+      )}
+      <SearchResults loading={loading} error={error} data={data} />
     </div>
   );
 }
---
+++ src/components/searchResults/searchResults.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../../assets/styles/searchResults.css';
+
+function SearchResults({ loading, error, data }) {
+  if (loading) return <p>Buscando...</p>;
+  if (error) return <p>Erro :( Tente novamente</p>;
+
+  return (
+    <div className="results-container">
+      {data &&
+        data.searchLegalCases.map((caseDetail) => (
+          // Each case detail is wrapped in a div container with a unique key
+          <div key={caseDetail.cnjNumber} className="case-container">
+            <h2 className="case-header">Processo nº {caseDetail.cnjNumber}</h2>
+            <div className="case-details">
+              <span className="case-detail">
+                Tribunal: {caseDetail.originCourt}
+              </span>
+              <span className="case-detail">
+                Partes Envolvidas: {caseDetail.partyNames}
+              </span>
+            </div>
+            <h3 className="case-header">Movimentações</h3>
+            <ul className="case-movements">
+              {caseDetail.movements.map((movement, index) => (
+                // List item for each movement with a unique key
+                <li key={index} className="case-movement">
+                  <p className="case-detail">
+                    {movement.date} - {movement.description}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default SearchResults;
